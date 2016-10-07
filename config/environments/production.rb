6a517e8ffccc102cf281530f1d85add3a0ac4f59
Andrés Da Viá <andresi.davia@gmail.com>
2016-10-07 16:58:37 -0500

    Se cambiaron variables de entorno

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,15 +87,14 @@ Rails.application.configure do
   # Mail config
   config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.delivery_method = :smtp
-
   # SMTP settings for mailgun
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'sleepy-hollows-36899.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'sleepy-hollows-36899.herokuapp.com',
+    :enable_starttls_auto => true
   }
 end
