8c1cfed09e443b038d27ec5e897ebeb82daad121
Andrés Da Viá <andresi.davia@gmail.com>
2016-10-07 16:21:14 -0500

    Se cambiaron variables de entorno

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,11 @@ Rails.application.configure do
 
   # SMTP settings for mailgun
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
-    :user_name      => ENV['username'],
-    :password       => ENV['password'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'sleepy-hollows-36899.herokuapp.com', #eg: 'yourappname.herokuapp.com'
     :authentication => :plain,
   }
 end
