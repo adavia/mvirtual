52631ca6e81b52ef29ee63b6707afc6496f6b9c2
Andrés Da Viá <andresi.davia@gmail.com>
2016-10-10 17:07:34 -0500

    Se agrego favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "contacts#new"
 
-  resources :contacts
+  resources :contacts, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
